401a9bc1febb8faf1800c848837d1b33101dfe42
lzxgogogo <lizixin_work@163.com>
2025-03-14 08:46:29 -0700

    modified:   Day5_Timer/map_Timer.cc

--- Day5_Timer/map_Timer.cc
+++ Day5_Timer/map_Timer.cc
@@ -2,91 +2,83 @@
 #include <functional>
 #include <chrono>
 #include <iostream>
-#include <set>
+#include <map>          // 改用 multimap
 #include <memory>
 using namespace std;
-
 
 struct TimerNodeBase {
     time_t expire;
     int64_t id;
+    // 需要提供 operator< 用于排序
+    bool operator<(const TimerNodeBase& other) const {
+        return (expire < other.expire) || (expire == other.expire && id < other.id);
+    }
 };
 
 struct TimerNode : public TimerNodeBase {
     using Callback = std::function<void(const TimerNode &node)>;
-    Callback func; 
-    
-    TimerNode(int64_t id,time_t expire, Callback func): func(func){
-        this->expire = expire;
-        this->id = id;
-    }
+    Callback func;
+
+    // 构造函数初始化基类成员
+    TimerNode(int64_t id, time_t expire, Callback func)
+        : TimerNodeBase{expire, id}, func(std::move(func)) {}
 };
-
-bool operator< (const TimerNodeBase &lhd, const TimerNodeBase &rhd){
-    if(lhd.expire < rhd.expire){
-        return true;
-    }else if(lhd.expire > rhd.expire){
-        return false;
-    }else {
-        return lhd.id < rhd.id;
-    }
-}
-
 
 class Timer {
 public:
     static time_t GetTick() {
-        auto sc = chrono::time_point_cast<chrono::milliseconds> (chrono::steady_clock::now());
-        auto temp = chrono::duration_cast<chrono::milliseconds> (sc.time_since_epoch());
-        return temp.count();
+        auto sc = chrono::time_point_cast<chrono::milliseconds>(chrono::steady_clock::now());
+        return chrono::duration_cast<chrono::milliseconds>(sc.time_since_epoch()).count();
     }
+
     static int64_t GenID() {
         return ++gid;
     }
-    // msec事件过后 ， 执行func
-    TimerNodeBase AddTimer(time_t msec, TimerNode::Callback func){
+
+    // 添加定时器
+    TimerNodeBase AddTimer(time_t msec, TimerNode::Callback func) {
         time_t expire = GetTick() + msec;
-        if(timeouts.empty() || expire <= timeouts.crbegin()->expire){
-            auto pairs = timeouts.emplace(GenID(), expire, std::move(func));
-            return static_cast<TimerNodeBase>(*pairs.first);
-        }
-        auto ele = timeouts.emplace_hint(timeouts.crbegin().base(), GenID(), expire, std::move(func));
-        return static_cast<TimerNodeBase>(*ele);
+        int64_t id = GenID();
+        TimerNode node(id, expire, std::move(func));
+        // 插入到 multimap（以 expire 为 key）
+        auto iter = timeouts.emplace(expire, std::move(node));
+        return static_cast<TimerNodeBase>(iter->second);
     }
 
-
-    bool DelTimer(TimerNodeBase &node){
-        // 加入到某个数据结构中 
-        auto iter = timeouts.find(node); 
-        if( iter != timeouts.end()){//找到 并删除
-            timeouts.erase(iter);
-            return true;
+    // 删除定时器
+    bool DelTimer(TimerNodeBase &node) {
+        // 查找相同 expire 的范围
+        auto range = timeouts.equal_range(node.expire);
+        for (auto it = range.first; it != range.second; ++it) {
+            if (it->second.id == node.id) {
+                timeouts.erase(it);
+                return true;
+            }
         }
         return false;
     }
-    
-    void HandleTimer(time_t now){
-        auto iter =  timeouts.begin();
-        while(iter != timeouts.end() && iter->expire <= now){
-            iter->func(* iter);
+
+    // 处理到期定时器
+    void HandleTimer(time_t now) {
+        auto iter = timeouts.begin();
+        while (iter != timeouts.end() && iter->first <= now) {
+            iter->second.func(iter->second); // 触发回调
             iter = timeouts.erase(iter);
         }
     }
-    
-    time_t TimeToSleep(){
-        // 从数据结构里取最小结点
-        auto iter = timeouts.begin();
-        if(iter == timeouts.end()){
-            return -1;
-        }
-        time_t diss = iter->expire - GetTick();
+
+    // 计算下次等待时间
+    time_t TimeToSleep() {
+        if (timeouts.empty()) return -1;
+        time_t diss = timeouts.begin()->first - GetTick();
         return diss > 0 ? diss : 0;
     }
+
 private:
     static int64_t gid;
-    set <TimerNode, std::less<>> timeouts;
-
+    multimap<time_t, TimerNode> timeouts; // 改用 multimap
 };
+
 int64_t Timer::gid = 0;
 
 
